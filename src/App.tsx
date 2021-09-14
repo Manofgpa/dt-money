37c3562391a00ad225b216f1df62400a0348a9b7
Felipe Mano <manofgpa@gmail.com>
2021-09-14 16:03:35 -0300

    Add modal global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import Modal from 'react-modal'
 import { Dashboard } from './components/Dashboard'
 import { Header } from './components/Header'
 import { NewTransactionModal } from './components/NewTransactionModal'
 import { GlobalStyle } from './styles/global'
-
-Modal.setAppElement('#root')
 
 function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
